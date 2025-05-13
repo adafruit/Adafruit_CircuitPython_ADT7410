bb16966e8dbf6e7b994cac41c35ed7f64b857a15
foamyguy <foamyguy@gmail.com>
2025-05-13 17:32:40 +0000

    change to ruff

--- examples/adt7410_operation_mode.py
+++ examples/adt7410_operation_mode.py
@@ -3,8 +3,9 @@
 # SPDX-License-Identifier: MIT
 
 import time
-import board
+
 import adt7410
+import board
 
 i2c = board.I2C()
 adt = adt7410.ADT7410(i2c)
@@ -15,6 +16,6 @@ while True:
     for operation_mode in adt7410.operation_mode_values:
         print("Current Operation mode setting: ", adt.operation_mode)
         for _ in range(10):
-            print("Temperature: {:.2f}C".format(adt.temperature))
+            print(f"Temperature: {adt.temperature:.2f}C")
             time.sleep(0.5)
         adt.operation_mode = operation_mode
